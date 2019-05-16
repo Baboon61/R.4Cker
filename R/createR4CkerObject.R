7197e1229004f74e7f5c95d224e99c5705573a76
Baboon61 <bastienherve61340@orange.fr>
2019-05-16 15:33:15 +0200

    check issue

--- R/createR4CkerObject.R
+++ R/createR4CkerObject.R
@@ -127,6 +127,9 @@ createR4CkerObjectFromFiles <- function(files,bait_chr,bait_coord,bait_name,prim
                       replicates = replicates,
                       species = species,
                       output_dir = output_dir)
+  print("Hello")
+  print(data_cis)
+  print(data_trans)
   print(obj_4Cker)
   return(obj_4Cker)
 }
